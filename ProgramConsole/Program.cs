48dd182ed343e00534985e88f951dd6009bb9faf
Fastom777 <dimapokhomov245@mail.ru>
2025-06-16 21:00:10 +0300

    learn c#

---
+++ ProgramConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Укажите ваш возраст: ");
+        string? age = Console.ReadLine(); // Читка ответа пользователя
+        Console.WriteLine($"Вам {age} лет!");
+
+
+        Console.WriteLine("Добро пожаловать!");
+        Console.Write("Укажите первое число: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Укажите второе число: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        int result;
+        result = num1 + num2;
+        Console.WriteLine($"Ваш результат: {result}");
+    }
+}
