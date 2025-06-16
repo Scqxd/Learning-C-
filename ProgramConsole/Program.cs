c998a50d625f410730c006763c5c7ceff02d4712
Fastom777 <dimapokhomov245@mail.ru>
2025-06-16 22:23:12 +0300

    learn c#

--- ProgramConsole/Program.cs
+++ ProgramConsole/Program.cs
@@ -1,23 +1,14 @@
-﻿
-using System;
-
-internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Console.Write("Укажите ваш возраст: ");
-        string? age = Console.ReadLine(); // Читка ответа пользователя
-        Console.WriteLine($"Вам {age} лет!");
+﻿Console.Write("Укажите ваш возраст: ");
+string? age = Console.ReadLine(); // Читка ответа пользователя
+Console.WriteLine($"Вам {age} лет!");
 
 
-        Console.WriteLine("Добро пожаловать!");
-        Console.Write("Укажите первое число: ");
-        int num1 = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Укажите второе число: ");
-        int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Добро пожаловать!");
+Console.Write("Укажите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Укажите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
 
-        int result;
-        result = num1 + num2;
-        Console.WriteLine($"Ваш результат: {result}");
-    }
-}
+int result;
+result = num1 + num2;
+Console.WriteLine($"Ваш результат: {result}");
