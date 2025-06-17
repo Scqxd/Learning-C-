54bc9e291ce87dc29549e60a1be0d37fe609b1f6
Fastom777 <dimapokhomov245@mail.ru>
2025-06-17 18:11:08 +0300

    learn c#

--- ProgramConsole/Program.cs
+++ ProgramConsole/Program.cs
@@ -45,7 +45,7 @@ Console.WriteLine($"r={r}  k={k}  c={c}  d={d}");
 double love = 10 / (5 * 2); //Для обозначения приоритетности добавлять скобки!!!
 Console.WriteLine(love);
 
-//Операции присваивания
+//Операции присвоения
 
 int num5, num6, num7;
 num5 = num6 = num7 = 228;
@@ -56,7 +56,7 @@ sum1 = sum2 = 25;
 sum3 = sum1 * sum2;
 Console.WriteLine($"{sum3}");
 
-int ram1 = 1;
+int ram1 = 0;
 int ram2 = 0;
 
 Console.WriteLine($"Напишите число ram1 и я его переобразую во вторую переменную ram2\n(Пока Ram2 - {ram2}, Ram1 - {ram1})!!!");
@@ -66,3 +66,39 @@ ram1 = Convert.ToInt32(Console.ReadLine());
 ram2 = ram1;
 
 Console.WriteLine($"На основе вашего вписанного значения Ram1 = {ram1}, Ram2 = {ram2}");
+
+//Применение операций присвоения:
+
+int h = 10;
+h += 10;        // 20
+h -= 4;         // 16
+h *= 2;         // 32
+h /= 8;         // 4
+h <<= 4;      // 64
+h >>= 2;      // 16
+
+Console.WriteLine(h);
+
+int l = 1;
+l += 1; // 2
+l += 2; // 4
+
+Console.WriteLine(l);
+
+//Операции сравнения
+
+int pop = 5;
+int pok = 6;
+int lok = 5;
+
+bool sf1 = pop == pok;
+bool sf2 = pop == lok;
+bool sf3 = pop > pok;
+bool sf4 = pop < pok;
+bool sf5 = lok < pop;
+bool sf6 = lok > pop;
+bool sf7 = pop <= lok;
+bool sf8 = pop >= lok;
+bool sf9 = pop <= pok;
+
+Console.WriteLine($"sf1 - {sf1}, sf2 - {sf2}, sf3 - {sf3}\nsf4 - {sf4}, sf5 - {sf5}, sf6 - {sf6}\nsf7 - {sf7}, sf8 - {sf8}, sf9 - {sf9}");
